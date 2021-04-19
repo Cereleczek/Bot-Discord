6453a710a888168d04f287bdabb53b5dd4f3fe76
Cerel <skuzajan@gmail.com>
2021-04-19 13:50:01 +0200

    Aktualna wersja
    Gotowy bot z komendami wyświetlania, i dodawania + komenda help

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const config = require("./config/config.js");
 const chalk = require("chalk");
 const Discord = require('discord.js');
-const sqlcon = require('./mySQL/connect');
-const configjson = require('C:/Users/skuza/Desktop/Glowne Projekty/Discord-Bot/config.json');
-
 
 let prefix = '%';
 let licznik = 0;
@@ -29,7 +26,7 @@ client.on('ready', () => {
 client.on('message', (msg) => {
 
   const {author} = msg;
-  //const channel = client.channels.cache.get('534842938560217108');
+
 module.exports = { msg};
 
 if (msg.content === 'prefix') { 
@@ -43,35 +40,12 @@ const command = args.shift().toLowerCase();
 
 if (!client.commands.has(command)) console.log('no such command');
 try {
-    
-  let sqlcon = mysql.createConnection(configjson.mysql);
-
-  sqlcon.connect(err => {
-
-    if (err) return console.log('Can\'t connect to database');
-
-    console.log('MySQL has been connected!');
-
-    sqlcon.query('SELECT * FROM panstwamiasta', (err, result, fields) => {
-    
-      let data = result.map(v => {
-        //console.log(v.userID)
-        
-        msg.channel.send(`${v.ID} ${v.Panstwa} ${v.Miasta}`)
-      });
-
-    // data = JSON.stringify(result);
-    // msg.channel.send(data);
-    
-    //console.log(data);
-  
-    });
 
     console.log(msg.content + ' ' + msg.author.tag + ' ' + msg.guild.channels.find(
       channel => channel.name.toLowerCase()
     ));    //client.channels pokazuje kanały
     client.commands.get(command).execute(msg, args);
-  });
+  //});
 } catch (error) {
     console.error(error);
     msg.reply('Error');
